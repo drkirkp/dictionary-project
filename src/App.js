260243166d45f5b8e9cbf53660bb9faf2a7cdcf1
drkirkp <154014621+drkirkp@users.noreply.github.com>
2024-05-05 13:51:35 -0500

    added GitHub and Netlify links

--- src/App.js
+++ src/App.js
@@ -9,7 +9,16 @@ function App() {
         <main>
           <Dictionary />
         </main>
-        <footer className="App-footer">Coded by Dail Williams</footer>
+        <footer className="App-footer">
+          This project was coded by Dail Williams and is{" "}
+          <a href="https://github.com/drkirkp/dictionary-project">
+            open-sourced on Github
+          </a>{" "}
+          and{" "}
+          <a href="https://jovial-dasik-26072b.netlify.app/">
+            hosted on Netlify
+          </a>
+        </footer>
       </div>
     </div>
   );
