2ec1883b7962c56c3fc4164d893f47ed0d916bff
drkirkp <154014621+drkirkp@users.noreply.github.com>
2024-05-05 10:36:35 -0500

    Setup Search Engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"> hello </header>
+      <div className="container">
+        <header className="App-header"> hello </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Dail Williams</footer>
+      </div>
     </div>
   );
 }
